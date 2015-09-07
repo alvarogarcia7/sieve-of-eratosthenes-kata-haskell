14bfabb6db9bb2bdeba73a3002e42837693bfaa4
Alvaro Garcia <josep.arpi+derecerca@gmail.com>
2015-09-07 20:19:09 +0200

    green

--- Spec.hs
+++ Spec.hs
@@ -8,6 +8,14 @@ main = hspec $ do
     describe "Sieve of Eratosthenes" $ do
         it "calculate the lower bound" $ do
           eratosthenes [2] `shouldBe` [2]
-  
+
+        it "calculate the first 10 primes" $ do
+          eratosthenes [2..29] `shouldBe` [2,3,5,7,11,13,17,19,23,29]
+
+
 eratosthenes :: [Int] -> [Int]
-eratosthenes all = []
+eratosthenes candidates = eratosthenes' [] candidates
+
+eratosthenes' :: [Int] -> [Int] -> [Int]
+eratosthenes' primes [] = primes 
+eratosthenes' primes (x:xs) = eratosthenes' (primes++[x]) (  filter (\i -> i `mod` x /= 0) xs )
